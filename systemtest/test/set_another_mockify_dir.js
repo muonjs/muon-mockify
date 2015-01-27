d044294bfcaaa8d38aba78c184cca4cef4c80122
Kreees <sukharevkirill@gmail.com>
2015-01-27 19:32:45 +0300

    add tests for mocks in  another dirs

--- systemtest/test/set_another_mockify_dir.js
+++ systemtest/test/set_another_mockify_dir.js
@@ -1,11 +1,54 @@
 describe("mockify.setMockifyDir & mockify.addMockifyDir",function(){
-    describe("test it",function(){
-        before(function(){
 
+    describe("should find secondmodule in mock_modules and thirdmodule in mock_modules2",function(){
+        var secondMock, thirdMock,
+            secondTest, thirdTest;
+
+        var secondModule = "../lib/secondmodule", thirdModule = "../lib/thirdmodule";
+        var secondMockModule = "../mock_modules/lib/secondmodule", thirdMockModule = "../mock_modules2/lib/thirdmodule";
+
+        before(function(){
+            secondMock = require(secondMockModule);
+            thirdMock = require(thirdMockModule);
+
+            mockify.enable();
+            mockify.addMockifyDir("./mock_modules2");
+            secondTest = require(secondModule);
+            thirdTest = require(thirdModule);
         });
 
-        it("should fail",function(){
-            (true).should.not.be.ok;
+        it("should find secondmodule in default mock_modules",function(){
+            secondTest.filename.should.be.eql(secondMock.filename);
+        });
+
+        it("should find thirdmodule in default mock_modules2",function(){
+            thirdTest.filename.should.be.eql(thirdMock.filename);
+        });
+    });
+
+    describe("should find secondmodule and thirdmodule in mock_modules2",function(){
+        var secondMock, thirdMock,
+            secondTest, thirdTest;
+
+        var secondModule = "../lib/secondmodule", thirdModule = "../lib/thirdmodule";
+        var secondMockModule = "../mock_modules2/lib/secondmodule", thirdMockModule = "../mock_modules2/lib/thirdmodule";
+
+        before(function(){
+            secondMock = require(secondMockModule);
+            thirdMock = require(thirdMockModule);
+
+            mockify.enable();
+            mockify.setMockifyDir("./mock_modules2");
+            secondTest = require(secondModule);
+            thirdTest = require(thirdModule);
+        });
+
+        it("should find secondmodule in default mock_modules2",function(){
+            secondTest.filename.should.be.eql(secondMock.filename);
+        });
+
+        it("should find thirdmodule in default mock_modules2",function(){
+            thirdTest.filename.should.be.eql(thirdMock.filename);
         });
     });
 });

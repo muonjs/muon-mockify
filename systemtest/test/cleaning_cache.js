7c2bc95a6eeacfb091501ba0aaa1b49e216747b0
Kreees <sukharevkirill@gmail.com>
2015-10-30 16:22:50 +0300

    set only mock-modules cache ceaning when enable/disable mockify

--- systemtest/test/cleaning_cache.js
+++ systemtest/test/cleaning_cache.js
@@ -1,7 +1,7 @@
-describe("cleaning cache",function(){
-    describe("should check whether cache cleaned on enable and disable",function(){
+describe("cleaning cache\n",function(){
+    describe("should check whether cache cleaned on enable and disable\n",function(){
         var RunTest = function(module){
-            describe("should be clean cache for: "+module,function(){
+            describe("should return the same module object after enable/disable: "+module+"\n",function(){
                 var original,mocked,cleaned;
 
                 before(function(){
@@ -13,23 +13,47 @@ describe("cleaning cache",function(){
                 });
 
                 it("should not match original and mocked",function(){
-                    (original === mocked).should.not.be.ok;
+                    (original === mocked).should.be.false();
                 });
 
                 it("should not match original and post disabled",function(){
-                    (original === cleaned).should.not.be.ok;
+                    (original === cleaned).should.be.true();
                 });
             });
+        };
 
+        var RunTestMocked = function(module){
+            describe("should clean cache for mock-modules: "+module+"\n",function(){
+                var original,mocked,cleaned;
+
+                before(function(){
+                    original = require(module);
+                    mockify.enable();
+                    mocked = require(module);
+                    mockify.disable();
+                    cleaned = require(module);
+                });
+
+                it("should not match original and mocked",function(){
+                    (original === mocked).should.be.false();
+                });
+
+                it("should not match original and post disabled",function(){
+                    (original === cleaned).should.be.false();
+                });
+            });
         };
 
         ([
             "../main",
-            "../lib/mymodule",
+            "../lib/mymodule"
+        ]).forEach(RunTest);
+
+        ([
             "../mock_modules/main",
             "../mock_modules/lib/mymodule",
             "../mock_modules/node_modules/foo",
             "../mock_modules/node_modules/foo/optional"
-        ]).forEach(RunTest);
+        ]).forEach(RunTestMocked)
     });
 });
